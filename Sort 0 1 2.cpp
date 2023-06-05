ac6551047e654a632595ddcdeb76f69478f4b6e6
JAYANT JAIN <56956286+jayant26@users.noreply.github.com>
2023-06-05 14:45:57 +0530

    Create Sort 0 1 2.cpp

---
+++ Sort 0 1 2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+void sort012(int *arr, int n)
+{
+     int l=0,m=0,h=n-1;
+        while(m<=h)
+        {
+            if(arr[m]==1)
+            {
+                m++;
+            }
+            else if(arr[m]==0)
+            {
+                swap(arr[l],arr[m]);
+                l++;
+                m++;
+            }
+            else
+            {
+                swap(arr[m],arr[h]);
+                h--;
+            }
+        }
+        return ;
+}
