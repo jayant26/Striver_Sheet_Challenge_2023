cdee9b4f36802f4e7a128dc674da1065b5745bed
JAYANT JAIN <56956286+jayant26@users.noreply.github.com>
2023-07-14 01:51:47 +0530

    Create Find Nth Root Of M.cpp

---
+++ Find Nth Root Of M.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+int NthRoot(int n, int m) {
+    int l=1,h=m;
+    while(l<=h)
+    {
+      int mid=(l+h)/2;
+      if(pow(mid,n)==m)return mid;
+      else if(pow(mid,n)>m)
+      {
+        h=mid-1;
+      }
+      else
+      {
+        l=mid+1;
+      }
+     
+
+    }
+     return -1;
+}
